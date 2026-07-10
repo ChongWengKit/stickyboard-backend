464917e3dee75edf1ffbf5263c15999dc45a2468
chong wengkit <wengkitchong87@gmail.com>
2026-07-10 14:03:31 +0800

    test action

--- src/routes/boardRoutes.ts
+++ src/routes/boardRoutes.ts
@@ -1,7 +1,7 @@
 import { Router } from "express";
 import { boardController } from "../controller/boardController.js";
 import { cronController } from "../controller/cronController.js";
-
+//test
 const router = Router();
 
 router.get("/board", boardController.getBoard);
